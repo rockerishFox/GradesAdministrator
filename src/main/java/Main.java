30577ae9be786f984132a0353ba452698ea7ffe5
RockerishFox <delia.csiki.dely@gmail.com>
2019-12-26 20:54:57 +0200

    connection to database <3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws ClassNotFoundException {
         System.out.println("Hello");
+        String connectionUrl = "jdbc:sqlserver://localhost;database=GradesAdministrator;integratedSecurity=true";
+
+        try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
+            String SQL = "SELECT TOP 10 * FROM Person.Contact";
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            // Iterate through the data in the result set and display it.
+            while (rs.next()) {
+                System.out.println(rs.getString("FirstName") + " " + rs.getString("LastName"));
+            }
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/domain/Student.java
@@ -0,0 +1,27 @@
+package domain;
+
+public class Student {
+    private int id;
+    private String nume;
+
+    public Student(int id, String nume) {
+        this.id = id;
+        this.nume = nume;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+}
