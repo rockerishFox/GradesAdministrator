bf0df8ada14b47a65279947f1f163805cd4f4339
RockerishFox <delia.csiki.dely@gmail.com>
2020-04-03 11:36:19 +0300

    update main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,39 @@
-import java.sql.*;
+import UI.UI;
+import repositories.*;
+import services.NotaService;
+import services.StudentService;
+import services.TemaService;
+import services.config.ApplicationContext;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
-        System.out.println("Hello");
-        String connectionUrl = "jdbc:sqlserver://localhost;database=GradesAdministrator;integratedSecurity=true";
 
-        try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
-            String SQL = "SELECT * FROM vw_students";
-            ResultSet rs = stmt.executeQuery(SQL);
+    public static void runConsole() throws IOException {
+        XMLRepositoryStudent studentRepo = new XMLRepositoryStudent(
+                ApplicationContext.getPROPERTIES().getProperty("data.studentsXML")
+        );
+        XMLRepositoryTema temaRepo = new XMLRepositoryTema(
+                ApplicationContext.getPROPERTIES().getProperty("data.temeXML")
+        );
+        XMLRepositoryNota notaRepo = new XMLRepositoryNota(
+                ApplicationContext.getPROPERTIES().getProperty("data.noteXML"),
+                ApplicationContext.getPROPERTIES().getProperty("data.studentsXML"),
+                ApplicationContext.getPROPERTIES().getProperty("data.temeXML")
+        );
+        StudentService studentService = new StudentService(studentRepo);
+        TemaService temaService = new TemaService(temaRepo);
+        NotaService notaService = new NotaService(notaRepo);
+        UI ui = new UI(studentService, temaService, notaService);
+        ui.run();
+    }
 
-            // Iterate through the data in the result set and display it.
-            while (rs.next()) {
-                System.out.println(
-                                rs.getInt("id") + " " +
-                                rs.getString("nume") + " " +
-                                rs.getString("prenume") + " " +
-                                rs.getString("profesor") + " " +
-                                rs.getString("grupa")
-                        );
-            }
-        }
-        // Handle any errors that may have occurred.
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public static void runGui(String[] args){
+        MainApp.main(args);
+    }
+
+    public static void main(String[] args) throws IOException {
+        //runConsole();
+        runGui(args);
     }
 }
